311d27641e0500a60347abd9f22c62a718bfc5e2
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-09-19 18:58:12 +0545

    adding routes and creating page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
+ import {BrowserRouter, Route, Routes} from 'react-router-dom';
+  import Profile from './pages/Profile';
+  import About from './pages/About';
+  import Home from './pages/Home';
+  import SignIn from './pages/SignIn';
+  import SignUp from './pages/SignUp';
 function App() {
    
   return (
-    <div >
-       hi
-    </div>
+    <BrowserRouter>
+    <Routes>
+ <Route path='/' element={<Home/>}/>
+ <Route path='/sign-in' element={<SignIn/>}/>
+ <Route path='/sign-up' element={<SignUp/>}/>
+ <Route path='/about' element={<About/>}/>
+ <Route path='/profile' element={<Profile/>}/>
+
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+       hi this is profile
+    </div>
+  )
+}
+
+export default Profile
