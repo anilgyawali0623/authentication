7a416c99a5b4300f74adb537b38c9ecbf8493b37
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-09-20 09:04:13 +0545

    adding middleware

--- api/index.js
+++ api/index.js
@@ -1,22 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
- import userRoutes from "./routes/user.route.js";
- import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
- mongoose.connect(process.env.MONGODB)
- .then(()=>{
-     console.log("connected succesfully")
- })
-.catch((err)=>{
-     console.log(err)
-})
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("connected succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
- app.use(express.json());
+app.use(express.json());
 app.listen(3000, () => {
   console.log(" this server is running");
 });
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
-
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
